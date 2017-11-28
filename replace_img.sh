0c761ff0fa4b2aa6be10ee35edfe503be0c1bcce
zeetum <timothy.blackburn@education.wa.edu.au>
2017-11-28 16:57:03 +0800

    Update replace_img.sh

--- replace_img.sh
+++ replace_img.sh
@@ -3,7 +3,7 @@ if [ $# != 3 ]
 then
     echo "usage: replace_img.sh <directory> <old_image_filename> <new_image_location>\ne.g.
     ./replace_img.sh /www/pictures old_image.png /www/new_image.jpg"
-	exit
+    exit
 fi
 
 find $1 -name $2 | sed 's/\(.*\)\/.*/\1/' | while read line
