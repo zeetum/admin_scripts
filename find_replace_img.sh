4e0404831bdaf0f398164ae4585b271f1f536595
zeetum <timothy.blackburn@education.wa.edu.au>
2017-11-28 13:21:23 +0800

    Update find_replace_img.sh

--- find_replace_img.sh
+++ find_replace_img.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "usage: png_to_jpg.sh <directory> <old_image_filename> <new_image_location>"
-find ~/image_dir $1 -name $2 | sed 's/\(.*\)\/.*/\1/' | while read line
+find $1 -name $2 | sed 's/\(.*\)\/.*/\1/' | while read line
 do
     rm $line"/"$2
     cp $3 $line
