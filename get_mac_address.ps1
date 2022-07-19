9eae6a6f18f1d66525017e6f866ff7fedb041232
zeetum <timothy.blackburn@education.wa.edu.au>
2022-07-19 14:19:45 +0800

    Update get_mac_address.ps1

--- get_mac_address.ps1
+++ get_mac_address.ps1
@@ -5,10 +5,7 @@
 $computers = Get-ADComputer -Filter * -SearchBase "OU=School Managed,OU=Computers,OU=E5070S01,OU=Schools,DC=indigo,DC=schools,DC=internal"
 
 foreach ($comp in $computers) {
-    #Add-Content C:\temp\test.txt 
-    
     $adaptors = Get-NetAdapter -Name * -CimSession $comp.name
-    Write-Host ($adaptors | Format-Table | Out-String)
     $info = $comp.name + "," + $adaptors.MacAddress
-    Write-Host $info
+    Add-Content D:\mac_addresses.csv $info
 }
