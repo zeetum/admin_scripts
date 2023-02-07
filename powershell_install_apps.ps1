334414ae662c6546111b875225daf6802c3ca1df
zeetum <timothy.blackburn@education.wa.edu.au>
2023-02-07 11:17:52 +0800

    Update powershell_install_apps.ps1

--- powershell_install_apps.ps1
+++ powershell_install_apps.ps1
@@ -1,12 +1,8 @@
-#Specify OU and Credentials
-$OUPath = "OU=School Managed,OU=Computers,OU=E5070S01,OU=Schools,DC=indigo,DC=schools,DC=internal"
-$credentials = Get-Credential
-
 # Get all available software
 $allApps = Get-WmiObject -query "select * from CCM_Application" -namespace "root\ccm\clientsdk"| Select Name, Id, Revision
 
-# Find the app you want
-$allApps | Format-Table -AutoSize
+# Uncomment to find the app you want
+# $allApps | Format-Table -AutoSize
 
 # Select the app
 $NAPBrowser = $allApps | Where-Object -Property Name -Like "*NAPLAN Locked Down Browser*"
