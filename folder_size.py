edbbc08459cb2c6ade8f35ed11bb6e04d6f4ae2a
zeetum <timothy.blackburn@education.wa.edu.au>
2023-03-15 14:04:37 +0800

    Update folder_size.py

--- folder_size.py
+++ folder_size.py
@@ -1,13 +1,16 @@
 import os
 
 def folder_size(source):
+        
         total_size = os.path.getsize(source)
         for item in os.listdir(source):
+                
             itempath = os.path.join(source, item)
             if os.path.isfile(itempath):
                 total_size += os.path.getsize(itempath)
             elif os.path.isdir(itempath):
                 total_size += folder_size(itempath)
+                
         return total_size
 
 size = folder_size("/home/tum/") / (1024*1024*1024)
