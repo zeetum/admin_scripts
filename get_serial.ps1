ffe809510ca9debcb9d5ce0bb851bdc5bc3f017a
zeetum <timothy.blackburn@education.wa.edu.au>
2023-05-22 17:26:28 +0800

    Update get_serial.ps1

--- get_serial.ps1
+++ get_serial.ps1
@@ -3,13 +3,13 @@ $model = (Get-WmiObject -Class win32_computersystem).Model
 
 switch ($model) {
     "Something" {
-        Write-Host "Lease 15/16"
+        Write-Host "Model: Lease 15/16"
     }
     "Something else" {
-        Write-Host "2022 School Owned"
+        Write-Host "Model: 2022 School Owned"
     }
     default {
-        Write-Host "Model Not Found"
+        Write-Host "Model: No Match"
     }
 }
 
