936eccde014e4a08c9c539eab64842ed78ab91c9
zeetum <timothy.blackburn@education.wa.edu.au>
2023-05-26 12:25:53 +0800

    Update get_serial.ps1

--- get_serial.ps1
+++ get_serial.ps1
@@ -2,11 +2,11 @@ $serial = (Get-WmiObject win32_bios).Serialnumber
 $model = (Get-WmiObject win32_computersystem).Model
 
 switch ($model) {
-    "Something" {
-        Write-Host "Model: Lease 15/16"
+    "20Q6S3N800" {
+        Write-Host "Model: Lenovo Lease 15/16"
     }
-    "Something else" {
-        Write-Host "Model: 2022 School Owned"
+    "20X2S3T200" {
+        Write-Host "Model: Lenovo 2022 School Owned"
     }
     default {
         Write-Host "Model: "$model
