adfdd0ab36cb906230301d0427cb89267b94ca00
zeetum <timothy.blackburn@education.wa.edu.au>
2023-06-27 14:35:06 +0800

    Update get_ou_computer_details.ps1

--- stocktake/get_ou_computer_details.ps1
+++ stocktake/get_ou_computer_details.ps1
@@ -1,31 +1,25 @@
 Import-Module .\Choose-ADOrganizationalUnit.psm1
-Import-Module .\GetLoginCredentials.psm1
 
-# Get Login Details
-$LoginCredentials = GetCredentials
-$DCAddress = $LoginCredentials['DCAddress'].split(".")
-$SiteCode = $DCAddress[0].substring(1, 4)
-$Dom = $DCAddress[1]
-$FullDomNme = $DCAddress[1..3] -join "."
-$username = $LoginCredentials['username']
-$password = $LoginCredentials['password']
-$creds = new-object -typename System.Management.Automation.PSCredential -argumentlist $Dom\$username, (ConvertTo-SecureString $password -AsPlainText -Force)
+# Site Details
+$SiteCode = "5070"
+$Colour = "indigo"
+$FullDomainName = "indigo.schools.internal"
 
 # Choose OU
-$LocalOU = "OU=School Managed,OU=Computers,OU=E"+$SiteCode+"S01,OU=Schools,DC="+$Dom+",DC=schools,DC=internal"
-$OUDetails = Choose-ADOrganizationalUnit -HideNewOUFeature -Domain $FullDomNme -Credential $creds -RootOU $LocalOU
+$LocalOU = "OU=School Managed,OU=Computers,OU=E"+$SiteCode+"S01,OU=Schools,DC="+$Colour+",DC=schools,DC=internal"
+$OUDetails = Choose-ADOrganizationalUnit -HideNewOUFeature -Domain $FullDomainName -RootOU $LocalOU
 $OU = $OUDetails.DistinguishedName
 $FileName = $OUDetails.Name + ".csv"
 Write-Host `n"OU: "$OU
 
 # Find all computers in the OU
-$ouEntry = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$OU", $username, $password)
+$ouEntry = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$OU")
 $searcher = New-Object System.DirectoryServices.DirectorySearcher($ouEntry)
 $searcher.Filter = "(objectClass=computer)"
 $results = $searcher.FindAll()
 Write-Host "Number of Computers: "$results.Count`n
 
-# Write computer details to a CSV with the name as the selected OU
+# Write computer details to console
 Set-Content $FileName "Hostname, Model, Serial"
 foreach ($result in $results) {
     $computerName = $result.Properties["name"][0]
