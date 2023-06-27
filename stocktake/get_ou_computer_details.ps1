216877e18fee03ad23c713f68f725133a12fa557
zeetum <timothy.blackburn@education.wa.edu.au>
2023-06-27 09:49:54 +0800

    Update get_ou_computer_details.ps1

--- stocktake/get_ou_computer_details.ps1
+++ stocktake/get_ou_computer_details.ps1
@@ -11,3 +11,20 @@ $computers = Get-ADComputer -Filter * -SearchBase $ouDN
 foreach ($computer in $computers) {
     Write-Host "Computer Name: $($computer.Name)"
 }
+
+$serial = (Get-WmiObject win32_bios).Serialnumber
+$model = (Get-WmiObject win32_computersystem).Model
+
+switch ($model) {
+    "20Q6S3N800" {
+        Write-Host "Model: Lenovo Lease 15/16"
+    }
+    "20X2S3T200" {
+        Write-Host "Model: Lenovo 2022 School Owned"
+    }
+    default {
+        Write-Host "Model: "$model
+    }
+}
+
+Write-Host "Serial: "$serial
