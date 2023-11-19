b1d5807fc79ab6e1ab371272406904759bcf1d51
zeetum <timothy.blackburn@education.wa.edu.au>
2023-11-19 15:09:58 +0800

    Update powershell_reinstall_minecraft.ps1

--- program_management/powershell_reinstall_minecraft.ps1
+++ program_management/powershell_reinstall_minecraft.ps1
@@ -1,4 +1,11 @@
-#### Remove from computer  ####
+#### Get latest available from Software Centre ####
+# Get all available software
+$allApps = Get-WmiObject -query "select * from CCM_Application" -namespace "root\ccm\clientsdk"| Select Name, Id, Revision, IsMachineTarget
+
+# Select the app
+$Minecraft = $allApps | Where-Object -Property Name -Like "Minecraft Education"
+
+#### Remove old version from computer  ####
 $computer = $env:computername
 $UninstallRegKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
 
@@ -16,26 +23,26 @@ foreach ($regArch in @('Registry32', 'Registry64')) {
         
         # Find the NAP Browser to uninstall
         if($AppDisplayName -like "Minecraft Education") { 
-            $start = $AppUninstall.IndexOf("{")
-            $stop = $AppUninstall.IndexOf("}")
-            $UninstallString = $AppUninstall.substring($start, $stop - $start + 1)
-            $arguments = " /qn /x " + "'" + $UninstallString + "'"
-            $command = "msiexec" + $arguments
-            Invoke-Expression "$($command) | Out-Null"
+            # If the current version isn't the same as the available, remove it
+            if ($AppDetails.GetValue("DisplayVersion") -ne  $Minecraft.Revision) {
+                Write-Host "Current Version:" $AppDetails.GetValue("DisplayVersion")
+                Write-Host "Available Version" $Minecraft.Revision
+                break
 
-            Write-Host "Removed: "$AppDisplayName
+                $start = $AppUninstall.IndexOf("{")
+                $stop = $AppUninstall.IndexOf("}")
+                $UninstallString = $AppUninstall.substring($start, $stop - $start + 1)
+                $arguments = " /qn /x " + "'" + $UninstallString + "'"
+                $command = "msiexec" + $arguments
+                Invoke-Expression "$($command) | Out-Null"
+    
+                Write-Host "Removed: "$AppDisplayName
+            }
         }
     }
 }
 
-#### Install from Software Centre ####
-# Get all available software
-$allApps = Get-WmiObject -query "select * from CCM_Application" -namespace "root\ccm\clientsdk"| Select Name, Id, Revision, IsMachineTarget
-
-# Select the app
-$Minecraft = $allApps | Where-Object -Property Name -Like "Minecraft Education"
-
-# Install the app
+### Install the latest version ###
 ([wmiclass]'ROOT\ccm\ClientSdk:CCM_Application').Install($($Minecraft.Id), $($Minecraft.Revision), $($Minecraft.IsMachineTarget), 0, 'Normal', $False)
 
 if ($Minecraft) {
