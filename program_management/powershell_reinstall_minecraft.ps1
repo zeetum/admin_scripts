a02bb4ffc83833f9de9ef52de68c6446d542782b
zeetum <timothy.blackburn@education.wa.edu.au>
2023-11-20 14:35:07 +0800

    Update powershell_reinstall_minecraft.ps1

--- program_management/powershell_reinstall_minecraft.ps1
+++ program_management/powershell_reinstall_minecraft.ps1
@@ -1,10 +1,3 @@
-#### Get latest available from Software Centre ####
-# Get all available software
-$allApps = Get-WmiObject -query "select * from CCM_Application" -namespace "root\ccm\clientsdk"| Select Name, Id, Revision, IsMachineTarget
-
-# Select the app
-$Minecraft = $allApps | Where-Object -Property Name -Like "Minecraft Education"
-
 #### Remove old version from computer  ####
 $computer = $env:computername
 $UninstallRegKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
@@ -23,12 +16,6 @@ foreach ($regArch in @('Registry32', 'Registry64')) {
         
         # Find the NAP Browser to uninstall
         if($AppDisplayName -like "Minecraft Education") { 
-            # If the current version isn't the same as the available, remove it
-            if ($AppDetails.GetValue("DisplayVersion") -ne  $Minecraft.Revision) {
-                Write-Host "Current Version:" $AppDetails.GetValue("DisplayVersion")
-                Write-Host "Available Version" $Minecraft.Revision
-                break
-
                 $start = $AppUninstall.IndexOf("{")
                 $stop = $AppUninstall.IndexOf("}")
                 $UninstallString = $AppUninstall.substring($start, $stop - $start + 1)
@@ -37,13 +24,19 @@ foreach ($regArch in @('Registry32', 'Registry64')) {
                 Invoke-Expression "$($command) | Out-Null"
     
                 Write-Host "Removed: "$AppDisplayName
-            }
         }
     }
 }
 
-### Install the latest version ###
-([wmiclass]'ROOT\ccm\ClientSdk:CCM_Application').Install($($Minecraft.Id), $($Minecraft.Revision), $($Minecraft.IsMachineTarget), 0, 'Normal', $False)
+#### Get latest available from Software Centre ####
+# Get all available software
+$allApps = Get-WmiObject -query "select * from CCM_Application" -namespace "root\ccm\clientsdk"| Select Name, Id, Revision, IsMachineTarget, SoftwareVersion
+
+# Select the app
+$Minecraft = $allApps | Where-Object -Property Name -Like "Minecraft Education"
+
+# Install the latest version ###
+([wmiclass]'ROOT\ccm\ClientSdk:CCM_Application').Install($($Minecraft.Id), $($Minecraft.Revision), $($Minecraft.IsMachineTarget), 0, 'Normal', $False)| Out-Null
 
 if ($Minecraft) {
     Write-Host "Installed: " $Minecraft.Name
